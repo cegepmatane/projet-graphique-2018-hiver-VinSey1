7612b2bba32bd87e91dc5fd164bc1b180cc64dbb
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-01-31 15:26:41 -0500

    Mise à jour Règles. References #24

--- src/vue/VueRegles.java
+++ src/vue/VueRegles.java
@@ -9,6 +9,7 @@ import javafx.scene.layout.*;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.*;
  
@@ -18,13 +19,23 @@ public class VueRegles extends Application {
     public void start(Stage primaryStage) {
     	BorderPane pane = new BorderPane();
     	Label titre = new Label("Règles");
-    	titre.setFont(Font.font("Arial", FontWeight.BOLD, 80));
+    	VBox content = new VBox();
+        content.getChildren().add(new Label("Principe"));
+        content.getChildren().add(new Label("Déroulement Jour"));
+        content.getChildren().add(new Label("Déroulement Nuit"));
+        content.getChildren().add(new Label("Cartes"));
+        content.getChildren().add(new Label("Savoir-vivre"));
+        content.getChildren().add(new Label("Triche"));
+        TitledPane menu = new TitledPane("Catégories", content);
+        titre.setFont(Font.font("Arial", FontWeight.BOLD, 80));
     	titre.setAlignment(Pos.CENTER);
     	titre.setMaxWidth(Double.POSITIVE_INFINITY);
+    	menu.setExpanded(false);
     	pane.setTop(titre);
+    	pane.setCenter(menu);
         Scene scene = new Scene(pane, 1500, 750);
 
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Loup-garou");
         primaryStage.setScene(scene);
         primaryStage.show();
 
