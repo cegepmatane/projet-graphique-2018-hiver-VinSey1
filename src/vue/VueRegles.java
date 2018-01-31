c7e9f01d79677dd941fa24d489ecad01713ac9a0
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-01-31 16:10:24 -0500

    Mise à Jour VueRegles.java. References #24

--- src/vue/VueRegles.java
+++ src/vue/VueRegles.java
@@ -17,23 +17,45 @@ public class VueRegles extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-    	BorderPane pane = new BorderPane();
-    	Label titre = new Label("Règles");
-    	VBox content = new VBox();
-        content.getChildren().add(new Label("Principe"));
-        content.getChildren().add(new Label("Déroulement Jour"));
-        content.getChildren().add(new Label("Déroulement Nuit"));
-        content.getChildren().add(new Label("Cartes"));
-        content.getChildren().add(new Label("Savoir-vivre"));
-        content.getChildren().add(new Label("Triche"));
-        TitledPane menu = new TitledPane("Catégories", content);
-        titre.setFont(Font.font("Arial", FontWeight.BOLD, 80));
-    	titre.setAlignment(Pos.CENTER);
-    	titre.setMaxWidth(Double.POSITIVE_INFINITY);
-    	menu.setExpanded(false);
-    	pane.setTop(titre);
-    	pane.setCenter(menu);
-        Scene scene = new Scene(pane, 1500, 750);
+
+    	Text titre = new Text("Les Loups-Garous");
+    	titre.setFont(Font.font("Lucida Handwriting", 80));
+    	BorderPane.setAlignment(titre, Pos.CENTER);
+    	
+    	Button principe = new Button("Principe");
+    	Button deroulementJour = new Button("Déroulement Jour");
+    	Button deroulementNuit = new Button("Déroulement Nuit");
+    	Button cartes = new Button("Cartes");
+    	Button savoirVivre = new Button("Savoir-vivre");
+    	Button triche = new Button("Triche");
+    	
+    	VBox categories = new VBox();
+    	categories.getChildren().add(principe);
+    	categories.getChildren().add(deroulementJour);
+    	categories.getChildren().add(deroulementNuit);
+    	categories.getChildren().add(cartes);
+    	categories.getChildren().add(savoirVivre);
+    	categories.getChildren().add(triche);
+        
+        TitledPane menu = new TitledPane("Catégories", categories);
+        menu.setExpanded(false);
+    	menu.setMaxSize(170, 250);
+    	BorderPane.setAlignment(menu, Pos.BASELINE_LEFT);
+    	
+    	AnchorPane grille = new AnchorPane();
+    	
+    	AnchorPane.setTopAnchor(menu, 100.0);
+    	AnchorPane.setLeftAnchor(menu, 100.0);
+    	AnchorPane.setRightAnchor(menu, 100.0);
+    	grille.getChildren().add(menu);
+    	BorderPane miseEnPage = new BorderPane();
+    	grille.setMaxSize(1200, 600);
+        grille.setMinSize(100, 50);
+        grille.setStyle("-fx-border-color: black; -fx-border-width: 2px 2px 2px 2px");
+    	miseEnPage.setTop(titre);
+    	miseEnPage.setCenter(grille);
+    	
+        Scene scene = new Scene(miseEnPage, 1500, 750);
 
         primaryStage.setTitle("Loup-garou");
         primaryStage.setScene(scene);
