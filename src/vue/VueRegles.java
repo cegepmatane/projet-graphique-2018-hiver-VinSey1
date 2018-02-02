5c92939a06fff535fab74cf3206bbfefb938386c
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-02 15:48:38 -0500

    Mise à jour et finitions de la Vue Règles References #24

--- src/vue/VueRegles.java
+++ src/vue/VueRegles.java
@@ -21,13 +21,17 @@ public class VueRegles extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+    	//Création du titre avec sa police
     	Text titre = new Text("Les Loups-Garous");
     	titre.setFont(Font.font("Lucida Handwriting", 80));
+    	//Centre le Titre
     	BorderPane.setAlignment(titre, Pos.CENTER);
     	
+    	//Création du sous-titre avec sa police
     	Text sousTitre = new Text("Règles");
     	sousTitre.setFont(Font.font("Lucida Handwriting", 50));
-    	
+
+    	//Création des boutons du menu déroulant
     	Button principe = new Button("Principe");
     	Button deroulementJour = new Button("Déroulement Jour");
     	Button deroulementNuit = new Button("Déroulement Nuit");
@@ -35,18 +39,38 @@ public class VueRegles extends Application {
     	Button savoirVivre = new Button("Savoir-vivre");
     	Button triche = new Button("Triche");
     	
+    	//Création du bouton "Retour"
     	Button retour = new Button("Retour");
+    	//Paramètres de taille du bouton "Retour"
     	retour.setMaxWidth(200.0);
     	retour.setMinWidth(200.0);
     	
+    	//Création de l'explication des règles
     	Label contenu = new Label("Test, consectetur adipiscing elit. Praesent ipsum erat, imperdiet non tellus consectetur, "
     			+ "lobortis suscipit orci. Duis volutpat quam in interdum vestibulum. Maecenas at nibh aliquam, vulputate nisi quis, "
     			+ "pharetra orci. Aliquam quis purus semper tellus pulvinar posuere. Donec in massa porttitor, sodales tortor non, "
     			+ "pharetra risus. Praesent sit amet varius erat. Mauris sodales ultrices ullamcorper. Aliquam erat volutpat. Maecenas "
     			+ "mollis efficitur purus at euismod. Sed convallis leo vitae ligula blandit vehicula.");
+    	//Gestion de retour à la ligne du contenu
     	contenu.setWrapText(true);
+    	//Gestion de la taille du contenu
     	//contenu.setMaxWidth(950);
-
+    	contenu.setMaxSize(1000,400);
+    	contenu.setMinSize(1000,400);
+    	contenu.setAlignment(Pos.TOP_CENTER);
+    	contenu.setTranslateY(35.0);
+    	//Création de la zone de texte contenant le texte
+    	AnchorPane zoneTexte = new AnchorPane();
+    	//Gestion de la taille de la zone de texte
+    	zoneTexte.setMaxSize(1000,400);
+    	zoneTexte.setMinSize(1000,400);
+    	//Gestion du style de la zone de texte
+    	zoneTexte.setStyle("-fx-border-color: grey; -fx-border-width: 1px 1px 1px 1px; -fx-z-index: 10;");
+    	zoneTexte.setId("zoneTexteRegles");
+    	//Ajoute le contenu à la zone de texte
+    	zoneTexte.getChildren().add(contenu);
+    	
+    	//Création des catégories du menu
     	VBox categories = new VBox();
     	categories.getChildren().add(principe);
     	categories.getChildren().add(deroulementJour);
@@ -55,56 +79,62 @@ public class VueRegles extends Application {
     	categories.getChildren().add(savoirVivre);
     	categories.getChildren().add(triche);
         
+    	//Création du menu déroulant
     	TitledPane menu = new TitledPane("Catégories", categories);
+    	//Définit le menu comme étant fermé au lancement de l'application
         menu.setExpanded(false);
-        
-    	/*
-    	BorderPane.setAlignment(menu, Pos.BASELINE_LEFT);
+        //Gestion de la taille du menu
+    	menu.setMinSize(1000, 250);
+    	
+    	/*BorderPane.setAlignment(menu, Pos.BASELINE_LEFT);
     	menu.setStyle("-fx-z-index: 1; -fx-width:170px");*/
     	
+        //Création du groupe contenant le menu et la zone de texte
     	Group groupe = new Group();
-    	
-    	AnchorPane fenetre = new AnchorPane();
-    	AnchorPane zoneTexte = new AnchorPane();
-    	zoneTexte.setMaxSize(1000,400);
-    	zoneTexte.setMinSize(1000,400);
-    	zoneTexte.setStyle("-fx-border-color: grey; -fx-border-width: 1px 1px 1px 1px; -fx-z-index: 10;");
-    	zoneTexte.setId("zoneTexteRegles");
-    	zoneTexte.toBack();
-    	
-    	
-    	
+    	//Ajoute la zone de texte et le menu au groupe
     	groupe.getChildren().addAll(zoneTexte, menu);
+    	//Définit la zone de texte à l'arrière dans le groupe
+    	zoneTexte.toBack();
+    	//Définit le menu à l'avant dans le groupe
     	menu.toFront();
-    	menu.setMinSize(1000, 250);
-
+    	
+    	//Création de la fenêtre contenant le sous texte et le groupe
+    	AnchorPane fenetre = new AnchorPane();
+    	//Gestion de la taille de la fenetre
+    	fenetre.setMaxSize(1200, 600);
+    	fenetre.setMinSize(100, 50);
+    	//Gestion du style de la fenetre
+    	fenetre.setStyle("-fx-border-color: black; -fx-border-width: 2px 2px 2px 2px; -fx-background-color: #F5F5F5;");
+    	fenetre.setId("fenetreRegles");
+    	//Ajoute le groupe, le bouton retour et le sous titre à la fenetre
+        fenetre.getChildren().addAll(groupe, retour, sousTitre);
+        
+    	//Gestion de tous les placements des différents objets dans les AnchorPanels
     	AnchorPane.setTopAnchor(groupe, 120.0);
     	AnchorPane.setLeftAnchor(groupe, 100.0);
     	AnchorPane.setTopAnchor(sousTitre, 10.0);
     	AnchorPane.setLeftAnchor(sousTitre, 500.0);
-    	AnchorPane.setRightAnchor(sousTitre, 50.0);
     	AnchorPane.setBottomAnchor(retour, 20.0);
     	AnchorPane.setLeftAnchor(retour, 500.0);
-    	AnchorPane.setTopAnchor(contenu, 10.0);
     	AnchorPane.setLeftAnchor(contenu, 10.0);
     	AnchorPane.setRightAnchor(contenu, 10.0);
-    	fenetre.setMaxSize(1200, 600);
-    	fenetre.setMinSize(100, 50);
-        
-        zoneTexte.getChildren().add(contenu);
-        fenetre.getChildren().addAll(groupe, retour, sousTitre);
+
+        //Création de la mise en page
     	BorderPane miseEnPage = new BorderPane();
-    	
-    	fenetre.setStyle("-fx-border-color: black; -fx-border-width: 2px 2px 2px 2px; -fx-background-color: #F5F5F5;");
-    	fenetre.setId("fenetreRegles");
+    	//Ajoute le titre en haut de la mise en page
     	miseEnPage.setTop(titre);
+    	//Ajoute la fenetre au centre de la mise en page
     	miseEnPage.setCenter(fenetre);
     	
+    	//Création de la scène contenant la mise en page et définition de sa taille
         Scene scene = new Scene(miseEnPage, 1500, 750);
+        //Importation du fichier CSS pour la gestion de styles
         scene.getStylesheets().add(VueRegles.class.getResource("../decoration/lg.css").toExternalForm());
-
+        //Gestion du titre de la scene
         primaryStage.setTitle("Loup-garou");
+        //Définit la scene
         primaryStage.setScene(scene);
+        //Affiche la scene
         primaryStage.show();
 
     }
