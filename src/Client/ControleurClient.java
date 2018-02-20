c268a47d1fc5994b424182272759da3df4e85beb
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-20 16:53:48 -0500

    ajout d'un contact serveur references #42

--- src/Client/ControleurClient.java
+++ src/Client/ControleurClient.java
@@ -5,16 +5,26 @@ import vue.VueSalonDeJeu;
 public class ControleurClient {
 
 	VueSalonDeJeu vueSalonDeJeu;
+	ContactServeur contactServeur;
 	
 	public ControleurClient (VueSalonDeJeu vueSalonDeJeu) {
 		this.vueSalonDeJeu = vueSalonDeJeu;
+		this.contactServeur = contactServeur;
+		
 	}
 	
 	public void traiter(String message) {
 		if (message.equals("Demande vote du village")) {
 			vueSalonDeJeu.ajouterTexteAuChat("A vous de voter villageois");
 		}
+		
+		if (message.equals("Loup-garou tué")) {
+			vueSalonDeJeu.ajouterTexteAuChat("Un loup-garou a été tué");
+			//ueSalonDeJeu.modifierIndicationNombreDeLoupGarouVivant(nouveauNombreDeLoupGarou);
+		}
 	}
 	
-	
+	public void validerVote() {
+		contactServeur.envoyerMessage("Un joueur a cliqué sur voter");
+	}
 }
