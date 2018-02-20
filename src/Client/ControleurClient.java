8249767e4dd6fd46e18ed72b62ffb6024df099d8
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-02-20 16:47:32 -0500

    ajout du premier cas : demande vote au village references #42

--- src/Client/ControleurClient.java
+++ src/Client/ControleurClient.java
@@ -6,8 +6,14 @@ public class ControleurClient {
 
 	VueSalonDeJeu vueSalonDeJeu;
 	
+	public ControleurClient (VueSalonDeJeu vueSalonDeJeu) {
+		this.vueSalonDeJeu = vueSalonDeJeu;
+	}
+	
 	public void traiter(String message) {
-		
+		if (message.equals("Demande vote du village")) {
+			vueSalonDeJeu.ajouterTexteAuChat("A vous de voter villageois");
+		}
 	}
 	
 	
