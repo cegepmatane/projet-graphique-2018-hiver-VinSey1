a297f4e24199f1d48be76f2a1480ad9e232a85ef
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-20 16:54:54 -0500

    Classe de lancement de la connexion Client/Serveur References #43

--- src/Client/main.java
+++ src/Client/main.java
@@ -1,9 +1,15 @@
 package Client;
 
+import vue.VueSalonDeJeu;
+
 public class main {
 	
 	public static void main(String[] args) {
+		VueSalonDeJeu client = new VueSalonDeJeu();
+		ContactServeur contactserveur = new ContactServeur();
+		ControleurClient controleur = new ControleurClient(client);
 		
+		contactserveur.setControleur(controleur);
 	}
 
 }
