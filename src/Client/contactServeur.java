b0a24f70fea8c8b9c252f4295b76a88afca02e01
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-20 16:45:48 -0500

    Méthode attendreMessage() References #43

--- src/Client/contactServeur.java
+++ src/Client/contactServeur.java
@@ -13,13 +13,16 @@ public class contactServeur {
 	Socket connexion;
 	PrintWriter sortie;
 	BufferedReader lecture;
+	ControleurClient controleur;
 	
 	@SuppressWarnings("resource")
-	private void connection() {
+	public void connection() {
 		try {
 			connexion = new Socket(InetAddress.getLocalHost(), 11);
 			sortie = new PrintWriter(connexion.getOutputStream(), true);
 			lecture = new BufferedReader(new InputStreamReader(connexion.getInputStream()));
+			attendreMessage();
+			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -27,6 +30,32 @@ public class contactServeur {
 		}
 	}
 	
+	private void attendreMessage() {
+		
+	Thread thread = new Thread(new Runnable() {			
+		@Override
+		public void run() {
+			String message = "";
+			try {
+				while ((message = lecture.readLine()) != null) {
+					controleur.traiter(message);
+				}
+			} catch(Exception e) {}
+		}
+	});
+	thread.start();
+	}
+
+	public ControleurClient getControleur() {
+		return controleur;
+	}
+
+	public void setControleur(ControleurClient controleur) {
+		this.controleur = controleur;
+	}
 	
+	public void envoyerMessage(String message) {
+		
+	}
 
 }
