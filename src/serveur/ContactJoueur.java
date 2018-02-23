4b00ac2c5b7b9cd227c12c5993ee8b715c0f4bd1
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-02-23 13:19:24 -0500

    Ajout fichiers serveur

---
+++ src/serveur/ContactJoueur.java
@@ -0,0 +1,58 @@
+package serveur;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.io.*;
+import java.net.*;
+
+public class ContactJoueur implements Runnable  {
+
+	Socket connexion = null;
+	BufferedReader lecteur = null;
+	PrintWriter imprimante = null;
+
+
+	Serveur serveur = null;
+	
+	public ContactJoueur(Socket connexion, Serveur serveur)
+	{
+		
+
+		this.serveur = serveur;
+		this.connexion = connexion;
+		try {
+			this.imprimante = new PrintWriter(connexion.getOutputStream(), true);
+			this.lecteur = new BufferedReader(new InputStreamReader(connexion.getInputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {		
+		
+		if(lecteur == null) return;
+		String message;
+		try {
+			
+			while((message = lecteur.readLine()) != null)
+			{
+				serveur.traiter(message);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+		
+	public boolean envoiMessage(String message)
+	{
+		imprimante.println(message);
+		return true;
+	}		
+	
+	public PrintWriter getImprimante() {
+		return imprimante;
+	}
+	
+}
---
+++ src/serveur/main.java
@@ -0,0 +1,9 @@
+package serveur;
+
+public class main {
+	public static void main(String[] args) {
+		Serveur serveur = new Serveur();
+		serveur.ecouter();
+
+	}
+}
