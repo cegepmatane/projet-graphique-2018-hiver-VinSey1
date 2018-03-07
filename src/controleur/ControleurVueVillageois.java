d0eb2ee85ed79256e7da1a7a0e651c4db2f49371
Eliott BALTZ <31962428+Ace578@users.noreply.github.com>
2018-03-07 17:37:33 -0500

    modification de traiter pour qu'elle prenne en compte le traitement des XML references #49

--- src/controleur/ControleurVueVillageois.java
+++ src/controleur/ControleurVueVillageois.java
@@ -2,8 +2,18 @@ package controleur;
 
 import vue.VueVillageois;
 
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
 
 import javafx.application.Platform;
 import reseau.ContactServeur;
@@ -48,6 +58,30 @@ public class ControleurVueVillageois {
 				vueVillageois.setChoixJoueur(listeJoueurs);
 				
 			}
-		});		
+		});	
+		
+		try {
+			DocumentBuilder lecteurXML = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			
+			InputSource inputSource = new InputSource();
+			inputSource.setCharacterStream(new StringReader(message));
+			
+			Document doc = lecteurXML.parse(inputSource);
+			NodeList contenuMessage = doc.getElementsByTagName("message");		
+			
+		}catch(org.xml.sax.SAXException e){
+			e.printStackTrace();
+			
+		}
+		catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+			
+			
+		}
+		
 	}
+	
 }
